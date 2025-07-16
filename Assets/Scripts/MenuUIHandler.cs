2985eee7f77681ac192635062c3cee7f2a754bc9
Nathaniel Grey <itsnat381@gmail.com>
2025-07-16 18:14:50 +0100

    Can change colours on the forklift again. Need to make it persist again

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -14,7 +14,7 @@ public class MenuUIHandler : MonoBehaviour
 
     public void NewColorSelected(Color color)
     {
-        // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
     }
 
     private void Start()
@@ -29,7 +29,7 @@ public class MenuUIHandler : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-      public void Exit()
+    public void Exit()
     {
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
